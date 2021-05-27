94b6f1efa389e49fc20bf92cc6d08d2f3ab47514
poornesh-pH <poornesh9626@gmail.com>
2021-05-27 13:40:42 +0530

    2 files modified

--- src/Thinking-in-React/ProductList.js
+++ src/Thinking-in-React/ProductList.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export default function ProductList() {
-  return <div>Product List</div>;
+  return (
+    <div>
+      <h6>Shopping Goods</h6>
+      <ul class="collection">
+        <li class="collection-item">Alwin<div className="secondary-content">  50</div></li>
+        <li class="collection-item">Alvin</li>
+        <li class="collection-item">Alvin</li>
+        <li class="collection-item">Alvin</li>
+      </ul>
+      <h6>Electronics</h6>
+      <ul class="collection">
+        <li class="collection-item">Alvin</li>
+        <li class="collection-item">Alvin</li>
+        <li class="collection-item">Alvin</li>
+        <li class="collection-item">Alvin</li>
+      </ul>
+    </div>
+  );
 }
